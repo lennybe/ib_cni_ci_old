5eb2612d7d28222ad361fc6094df1e98d8985170
Lenny Verkhovsky <lennyb@mellanox.com>
2020-03-08 09:42:34 +0200

    ib_cni_ci scripts

--- ib_cni_install.sh
+++ ib_cni_install.sh
@@ -266,10 +266,10 @@ EOF
 
 function create_vfs {
     if [ $SRIOV_INTERFACE == 'auto_detect' ]; then
-        export SRIOV_INTERFACE=$(ls -l /sys/class/net/ | grep $(lspci |grep Mellanox | grep MT27800|head -n1|awk '{print $1}') | awk '{print $9}')
+        export SRIOV_INTERFACE=$(ls -l /sys/class/infiniband/ | grep $(lspci |grep Mellanox |head -n1|awk '{print $1}') | awk '{print $9}')
     fi
-    echo 0 > /sys/class/net/$SRIOV_INTERFACE/device/sriov_numvfs
-    echo $VFS_NUM > /sys/class/net/$SRIOV_INTERFACE/device/sriov_numvfs
+    echo 0 > /sys/class/infiniband/$SRIOV_INTERFACE/device/sriov_numvfs
+    echo $VFS_NUM > /sys/class/infiniband/$SRIOV_INTERFACE/device/sriov_numvfs
 }
 
 
